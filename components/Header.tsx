1a7d80d8ee262ece0c32220dd17412e2237857de
Zhicheng Gong <109425to@gmail.com>
2025-02-05 23:52:35 +0800

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import { motion, AnimatePresence } from "framer-motion";
 export default function Header() {
   const [isMobile, setIsMobile] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [logoChange, setIconChange] = useState(false);
 
   useEffect(() => {
     if (menuOpen) {
@@ -18,7 +19,7 @@ export default function Header() {
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth < 800) {
+      if (window.innerWidth < 640) {
         setIsMobile(true);
       } else {
         setIsMobile(false);
@@ -31,15 +32,38 @@ export default function Header() {
     };
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIconChange(true);
+      } else {
+        setIconChange(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="flex flex-row bg-white items-center sticky p-4 pl-7 pr-7 z-20 top-0">
+    <header className="flex flex-row bg-white items-center fixed p-3 px-7 z-20 top-0 w-full">
       <Link href={"/"}>
-        <Image
-          src="/text-logo.svg"
-          width={70}
-          height={100}
-          alt="lyhs-plus-text-icon"
-        />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={logoChange ? "logo" : "text-logo"}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Image
+              src={logoChange ? "./logo.svg" : "/text-logo.svg"}
+              width={logoChange ? 30 : 70}
+              height={logoChange ? 50 : 100}
+              alt="lyhs-plus-text-icon"
+            />
+          </motion.div>
+        </AnimatePresence>
       </Link>
       {isMobile ? (
         <>
@@ -63,12 +87,15 @@ export default function Header() {
         </>
       ) : (
         <>
-          <ul className="list-none p-0 ml-auto flex flex-row">
-            <li className="m-2 border-b border-transparent hover:border-b-black font-bold">
+          <ul className="list-none p-0 ml-auto flex flex-row mr-0">
+            <li className="border-b border-transparent p-2 px-4 rounded-full hover:bg-zinc-100 font-bold transition-all">
               <Link href={"/"}>首頁</Link>
             </li>
-            <li className="m-2 border-b border-transparent hover:border-b-black font-bold">
-              <Link href={"/join-beta"}>加入測試版</Link>
+            <li className="border-b border-transparent p-2 px-4 rounded-full hover:bg-zinc-100 font-bold transition-all">
+              <Link href={"/fix"}>功能介紹</Link>
+            </li>
+            <li className="border-b border-transparent p-2 px-4 rounded-full hover:bg-zinc-100 font-bold transition-all">
+              <Link href={"/fix"}>關於我們</Link>
             </li>
           </ul>
         </>
@@ -81,7 +108,7 @@ export default function Header() {
             exit={{ opacity: 0.2 }}
             animate={{ opacity: 1 }}
             className={
-              "fixed left-0 bg-gray-100/85 flex-grow w-full h-dvh m-menu p-4 box-border top-0 z-10 backdrop-blur overflow-auto flex flex-col"
+              "fixed left-0 bg-gray-100/80 flex-grow w-full h-dvh m-menu p-4 box-border top-0 z-10 backdrop-blur overflow-auto flex flex-col"
             }
           >
             <ul className="text-center grow flex flex-col items-center justify-center font-custom gap-6 text-zinc-600">
@@ -123,30 +150,17 @@ export default function Header() {
                   delay: 0.2,
                 }}
               >
-                <a href={"/join-beta"}>加入測試版</a>
-              </motion.li>
-              <motion.li
-                className={"text-2xl"}
-                initial={{ opacity: 0, y: 30 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 120,
-                  damping: 20,
-                  delay: 0.2,
-                }}
-              >
                 <a href={"/fix"}>關於我們</a>
               </motion.li>
               <li className="absolute bottom-24 flex gap-5 items-center text-sm">
                 <a
-                  href="/"
+                  href="/fix"
                   className="p-2 px-3 bg-zinc-200 rounded-full hover:bg-zinc-300 transition-all font-custom font-semibold border border-zinc-300"
                 >
                   一般登入
                 </a>
                 <a
-                  href="/"
+                  href="https://admin.lyhsca.org"
                   className="p-2 px-3 bg-zinc-200 rounded-full hover:bg-zinc-300 transition-all font-custom font-semibold border border-zinc-300"
                 >
                   管理者登入
